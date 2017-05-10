3fe922a63118d98d5124c95fd5e0bf3e0ce284eb
Carwyn Nelson <bacondrinker@gmail.com>
2017-05-10 20:33:12 +0100

    bit of shite refactoring like init@

--- LearningOpenGL/Main.cpp
+++ LearningOpenGL/Main.cpp
@@ -4,17 +4,50 @@
 // Game Libs
 #include "RenderEngine/Public/Display.h"
 
-int main()
+struct Model
 {
-	try
+	Model(GLuint vbo)
+	{
+		this->vbo = vbo;
+	}
+
+	GLuint getVbo()
+	{
+		return vbo;
+	}
+private:
+	GLuint vbo;
+};
+
+struct Loader
+{
+	Model loadData()
 	{
 		// An array of 3 vectors which represents 3 vertices
-		static const GLfloat vertices[] = {
+		const GLfloat vertices[] = {
 			-1.0f, -1.0f, 0.0f,
 			1.0f, -1.0f, 0.0f,
 			0.0f,  1.0f, 0.0f,
 		};
 
+		GLuint vaoId;
+		glGenVertexArrays(1, &vaoId);
+		glBindVertexArray(vaoId);
+
+		GLuint vbo;
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		return Model(vbo);
+	}
+};
+
+int main()
+{
+	try
+	{
 		auto exitCallback = [](GLFWwindow* window, int key, int scancode, int action, int mode) {
 			if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
 				glfwSetWindowShouldClose(window, GL_TRUE);
@@ -27,20 +60,13 @@ int main()
 		// key bindings / callbacks
 		glfwSetKeyCallback(display.getWindow(), exitCallback);
 
-		GLuint vaoId;
-		glGenVertexArrays(1, &vaoId);
-		glBindVertexArray(vaoId);
-
-		GLuint vbo;
-		glGenBuffers(1, &vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		Loader loader;
+		auto model = loader.loadData();
 
 		while (!display.closing())
 		{
 			glEnableVertexAttribArray(0);
-			glBindBuffer(GL_ARRAY_BUFFER, vbo);
+			glBindBuffer(GL_ARRAY_BUFFER, model.getVbo());
 			glVertexAttribPointer(
 				0, // attribute 0
 				3, // 3 vertices
