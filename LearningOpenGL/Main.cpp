45ac48820a47aaac372017b749226edba123d352
Carwyn Nelson <bacondrinker@gmail.com>
2017-05-11 22:33:26 +0100

    pulled renderer out into its own class

--- LearningOpenGL/Main.cpp
+++ LearningOpenGL/Main.cpp
@@ -5,13 +5,13 @@
 #include "RenderEngine/Public/Display.h"
 #include "RenderEngine/Public/Model.h"
 #include "RenderEngine/Public/Loader.h"
+#include "RenderEngine/Public/Renderer.h"
 
 int main()
 {
 	try
 	{
-
-		std::vector<GLfloat> vertices = {
+		const std::vector<GLfloat> vertices = {
 			-0.5f,  0.5f, 0.0f,
 			-0.5f, -0.5f, 0.0f,
 			0.5f, -0.5f, 0.0f,
@@ -27,27 +27,20 @@ int main()
 		};
 
 		Display display(800, 600, "Learning OpenGL");
+		Loader loader;
+		Renderer renderer;
 
 		// NOTE: I would like to remove getWindow
 		// and find some way to encapsulate setting
 		// key bindings / callbacks
 		glfwSetKeyCallback(display.getWindow(), exitCallback);
 
-		Loader loader;
 		auto model = loader.loadData(vertices);
 
 		while (!display.closing())
 		{
-			glClearColor(0.1f, 0.2f, 0.3f, 1.0);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-			glBindVertexArray(model.getVaoId());
-			glEnableVertexAttribArray(0);
-
-			glDrawArrays(GL_TRIANGLES, 0, model.getVertexCount());
-
-			glDisableVertexAttribArray(0);
-			glBindVertexArray(0);
+			renderer.prepare();
+			renderer.render(model);
 
 			display.update();
 		}
