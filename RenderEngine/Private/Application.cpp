b11b2eeccb3d5161ef39818348649161e9cb0762
Carwyn Nelson <bacondrinker@gmail.com>
2017-07-03 21:03:06 +0100

    Move loading logic into its own class

--- RenderEngine/Private/Application.cpp
+++ RenderEngine/Private/Application.cpp
@@ -21,60 +21,34 @@ void Application::run()
 
 	try
 	{
+        std::vector<float> vertices = {
+                0.5f,  0.5f, 0.0f,  // top right
+                0.5f, -0.5f, 0.0f,  // bottom right
+                -0.5f, -0.5f, 0.0f,  // bottom left
+                -0.5f,  0.5f, 0.0f   // top left
+        };
+        std::vector<unsigned int> indices = {  // note that we start from 0!
+                0, 1, 3,   // first triangle
+                1, 2, 3    // second triangle
+        };
+
 		Display display(800, 600, "Learning OpenGL");
 		Renderer renderer;
-
         Shader shader(vertexShaderSource, fragmentShaderSource);
 
-		// set up vertex data (and buffer(s)) and configure vertex attributes
-		// ------------------------------------------------------------------
-		float vertices[] = {
-				0.5f,  0.5f, 0.0f,  // top right
-				0.5f, -0.5f, 0.0f,  // bottom right
-				-0.5f, -0.5f, 0.0f,  // bottom left
-				-0.5f,  0.5f, 0.0f   // top left
-		};
-		unsigned int indices[] = {  // note that we start from 0!
-				0, 1, 3,   // first triangle
-				1, 2, 3    // second triangle
-		};
-
-		unsigned int vbo, vao, ebo;
-		glGenVertexArrays(1, &vao);
-		glGenBuffers(1, &vbo);
-		glGenBuffers(1, &ebo);
-		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-		glBindVertexArray(vao);
-
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-
-		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-		glBindVertexArray(0);
+        Loader loader(vertices, indices);
 
 		while (!display.closing())
 		{
 			renderer.prepare();
 
 			glUseProgram(shader.getProgram());
-			glBindVertexArray(vao);
+			glBindVertexArray(loader.getVao());
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 			glBindVertexArray(0);
 
 			display.update();
 		}
-
-		glDeleteVertexArrays(1, &vao);
-		glDeleteBuffers(1, &vbo);
 	}
 	catch (std::exception& e)
 	{
--- RenderEngine/Public/Loader.h
+++ RenderEngine/Public/Loader.h
@@ -1,23 +1,60 @@
 #pragma once
 
-#include "Model.h"
 #include <vector>
 
 class Loader
 {
-public:
-	~Loader();
-	Model loadData(std::vector<GLfloat> vertices);
 private:
-	std::vector<GLuint> vaos;
-	std::vector<GLuint> vbos;
+    unsigned int vao, vbo, ebo;
+public:
+    Loader(std::vector<float> vertices, std::vector<unsigned int> indices)
+    {
+        glGenVertexArrays(1, &vao);
 
-	GLuint createVao();
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
 
-	void deleteVao();
+        // Make the vao active.
+        glBindVertexArray(vao);
 
-	void storeVboDataInAttributeList(
-		GLuint attributeNumber,
-		std::vector<GLfloat> vertices
-	);
+        // bind the VBO to the array buffer and send the vertices to it.
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * 4, vertices.data(), GL_STATIC_DRAW);
+
+        // bind the EBO to the element array buffer and send the indices to it.
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * 4, indices.data(), GL_STATIC_DRAW);
+
+        // attach the active buffers to the vertex attribute array (vao)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+        // enable the vao
+        glEnableVertexAttribArray(0);
+
+        // disable the vao
+        glBindVertexArray(0);
+    }
+
+    ~Loader()
+    {
+        glDeleteVertexArrays(1, &vao);
+
+        glDeleteBuffers(1, &vbo);
+        glDeleteBuffers(1, &ebo);
+    }
+
+    unsigned int getVao()
+    {
+        return vao;
+    }
+
+    unsigned int getVbo()
+    {
+        return vbo;
+    }
+
+    unsigned int getEbo()
+    {
+        return ebo;
+    }
 };
